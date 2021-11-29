0237c75c8a4774a5aa0e79d87212a72eb5c089c1
ivyrhoda <ivy.rhoda@student.moringaschool.com>
2021-11-29 15:52:37 +0300

    add contact form and design it

--- script.js
+++ script.js
@@ -16,32 +16,9 @@ $('#flexSwitchCheckChecked').on('change', function () {
 $('#btnCheckout').on('click', function () {
     $('#cardCheckout').fadeIn(1000)
     $('#orderSize').html($('#pizzaSize').find('option:selected').text())
-    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
-    var finalAmount = amount * +$('#pizzaQuantity').val()
-    var orderAmount = finalAmount
-    if($('#flexSwitchCheckChecked').is(':checked')){
-        orderAmount = finalAmount + 1000
-    }
-    $('#orderTotal').html(orderAmount)
-
-})
-
-$('#btnCheckout').on('click',function(){
-    $('#cardCheckout').fadeIn(1000)
     $('#orderCrust').html($('#pizzaCrust').find('option:selected').text())
-    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
-    var finalAmount = amount * +$('#pizzaQuantity').val()
-    var orderAmount = finalAmount
-    if($('#flexSwitchCheckChecked').is(':checked')){
-        orderAmount = finalAmount + 1000
-    }
-    $('#orderTotal').html(orderAmount)
-
-})
-
-$('#btnCheckout').on('click',function(){
-    $('#cardCheckout').fadeIn(1000)
     $('#orderTopping').html($('#pizzaTopping').find('option:selected').text())
+    $('#orderQuantity').html($('#pizzaQuantity').val())
     var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
     var finalAmount = amount * +$('#pizzaQuantity').val()
     var orderAmount = finalAmount
@@ -52,16 +29,7 @@ $('#btnCheckout').on('click',function(){
 
 })
 
-$('#btnCheckout').on('click',function(){
-    $('#cardCheckout').fadeIn(1000)
-    $('#orderNumber').html($('#pizzaQuantity').find('option:selected').text())
-    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
-    var finalAmount = amount * +$('#pizzaQuantity').val()
-    var orderAmount = finalAmount
-    if($('#flexSwitchCheckChecked').is(':checked')){
-        orderAmount = finalAmount + 1000
-    }
-    $('#orderTotal').html(orderAmount)
-
-})
-  
+function formValidation() {
+    var name = document.getElementById("name").value;
+    alert( name + " we have received your message thank you for your feedback");
+  }
